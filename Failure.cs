ec78625b948cddd9626e4121b22fe71b0b4d45dc
Gavin Hell <gmangavin1@gmail.com>
2016-08-15 00:44:56 -0700

    Update 9
    Failure CS
    •New class, all comments to show past fails.
    
    gameplay
    •Fixed annoying name bug, where if the function was recalled it would
    create multiple strings and values.

---
+++ Failure.cs
@@ -0,0 +1,101 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This is a past fail which caused the function to create a new string each time the function was recalled.                                                                  //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//public static string NameCheck()
+//{
+//    BTS.Clear();
+//    Console.Write("What name would you like: ");
+//    string R = Console.ReadLine();
+//
+//    while (true)
+//    {
+//        if (R.Length > 0)
+//        {
+//            foreach (char letter in R)
+//            {
+//                if (char.IsLetter(letter))
+//                {
+//                }
+//                else
+//                {
+//                    Console.WriteLine("Can only be letters");
+//                    BTS.ReWrite();
+//                    NameCheck();
+//                }
+//            }
+//            break;
+//        }
+//        else if (R.Length < 1)
+//        {
+//            Console.WriteLine("Must contain some letters");
+//            BTS.ReWrite();
+//            NameCheck();
+//        }
+//    }
+//
+//    BTS.Clear();
+//    System.Console.WriteLine("Are you sure you want to use the name: " + R + "?" + "[Yes or No]");
+//    while (true)
+//    {
+//        String R2 = Console.ReadLine();
+//        if (R2.ToLower() == "yes")
+//        {
+//            break;
+//        }
+//        else if (R2.ToLower() == "no")
+//        {
+//            NameCheck();
+//        }
+//    }
+//    return R;
+//}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This has a bug in which if a number was typed it would still continue to the [Yes / No] question.                                                                          //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//public static string NameCheck()
+//{
+//    string R = "";
+//
+//    while (true)
+//    {
+//        BTS.Clear();
+//        Console.Write("What name would you like: ");
+//        R = Console.ReadLine();
+//        if (R.Length > 0)
+//        {
+//            foreach (char letter in R)
+//            {
+//                if (char.IsLetter(letter))
+//                {
+//                }
+//                else
+//                {
+//                    Console.WriteLine("Can only be letters");
+//                    BTS.ReWrite();
+//                    continue;
+//                }
+//            }
+//            BTS.Clear();
+//            System.Console.WriteLine("Are you sure you want to use the name: " + R + "?" + "[Yes or No]");
+//            while (true)
+//            {
+//                String R2 = Console.ReadLine();
+//                if (R2.ToLower() == "yes")
+//                {
+//                    return R;
+//                }
+//                else if (R2.ToLower() == "no")
+//                {
+//                    break;
+//                }
+//            }
+//        }
+//        else if (R.Length < 1)
+//        {
+//            Console.WriteLine("Must contain some letters");
+//            BTS.ReWrite();
+//            continue;
+//        }
+//    }
+//}
--- gameplay.cs
+++ gameplay.cs
@@ -8,51 +8,50 @@ namespace CMDgame
 
         public static string NameCheck()
         {
-            BTS.Clear();
-            Console.Write("What name would you like: ");
-            string R = Console.ReadLine();
+            string R = ""; //Creat the string with an empty string value.
 
-            while(true)
+            while(true) //Runs until a return is made.
             {
-                if (R.Length > 0)
+                BTS.Clear();
+                Console.Write("What name would you like: "); 
+                R = Console.ReadLine(); //Sets a new value for the R string.
+                if (R.Length > 0) //if more than 0 characters are typed.
                 {
-                    foreach (char letter in R)
+                    foreach (char letter in R) //foreach character in the R string
                     {
-                        if (char.IsLetter(letter))
+                        if (char.IsLetter(letter)) //If each character is the R string is a letter.
                         {
                         }
                         else
                         {
                             Console.WriteLine("Can only be letters");
                             BTS.ReWrite();
-                            NameCheck();
+                            break; //breaks out of the foreach
                         }
+                        BTS.Clear();
+                        System.Console.WriteLine("Are you sure you want to use the name: " + R + "?" + "[Yes or No]");
+                        while (true)
+                        {
+                            String R2 = Console.ReadLine();
+                            if (R2.ToLower() == "yes")
+                            {
+                                return R; //Return string R.
+                            }
+                            else if (R2.ToLower() == "no")
+                            {
+                                break; //breaks from this while loop
+                            }
+                        }
+                        break; //When the foreach is broken out of break out of the main while loop.
                     }
-                    break;
                 }
-                else if (R.Length < 1)
+                else if (R.Length < 1) //If less than 1 character is typed.
                 {
                     Console.WriteLine("Must contain some letters");
                     BTS.ReWrite();
-                    NameCheck();
+                    continue; //Restart the while loop.
                 }
             }
-
-            BTS.Clear();
-            System.Console.WriteLine("Are you sure you want to use the name: " + R + "?" + "[Yes or No]");
-            while(true)
-            {
-                String R2 = Console.ReadLine();
-                if (R2.ToLower() == "yes")
-                {
-                    break;
-                }
-                else if (R2.ToLower() == "no")
-                {
-                    NameCheck();
-                }
-            }
-            return R;
         }
 
         public static void Adventure()
